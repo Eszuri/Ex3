aabce70833f44198bab1760d8429e804e48c7901
Eszuri <ualdo296@gmail.com>
2023-08-01 13:35:33 +0700

    1

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Set EJS as the view engine
-// app.set('view engine', 'ejs');
-// app.use(express.static("views", { index: "true" }));
-// app.use(express.static("views"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index'); // 'index' sesuai dengan nama file EJS (tanpa ekstensi .ejs)
+    res.render('index');
 });
 
 
 app.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
+    console.log(`http://localhost:${port} is Running`);
 });
